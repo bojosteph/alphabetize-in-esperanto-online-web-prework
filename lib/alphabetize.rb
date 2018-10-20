b285fb39e37428677e60935cd3ea2f7346c25d3a
Roberto Samia <bojosteph@gmail.com>
2018-10-20 02:26:21 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,8 +3,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by! do |x|
     letters = []
-    x.each_char do |char|
-      positions << ESPERANTO_ALPHABET.index(char)
+    x.each_char do |y|
+      letters << ESPERANTO_ALPHABET.index(char)
     end
     letters
   end
